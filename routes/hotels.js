dcc82dd91b15ef30d550f9a51e08f90a47b18d34
Oleg <kunasenkoolrg@gmail.com>
2023-04-10 20:06:01 +0300

    first commit

---
+++ routes/hotels.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotelRooms, getHotels, updateHotel } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import { verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyUser, createHotel);
+//UPDATE
+router.put("/:id", verifyUser, updateHotel);
+//DELETE
+router.delete("/:id", verifyUser, deleteHotel);
+//GET
+router.get("/find/:id", getHotel);
+//GET ALL
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+export default router
